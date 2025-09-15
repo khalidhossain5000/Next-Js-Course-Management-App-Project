1648d6779b14a322f2be912577a326488b31e2e0
khalidhossain <mdkhalidhossain5000@gmail.com>
2025-09-15 18:11:25 +0600

    title added

--- src/app/(Main)/Home/HomePage.jsx
+++ src/app/(Main)/Home/HomePage.jsx
@@ -6,7 +6,18 @@ import InfoSection from '@/app/Components/InfoSection/InfoSection';
 import Testimonial from '@/app/Components/TestimonialSLider/Testimonial';
 import TopCategories from '@/app/Components/TopCategories/TopCategories';
 import React from 'react';
-
+// app/page.tsx
+export const metadata = {
+  title: "Home | Dream LMS",
+  description:
+    "Welcome to Dream LMS Home. Browse and learn courses online with our full-stack Next.js course management app.",
+  openGraph: {
+    title: "Home | Dream LMS",
+    description:
+      "Welcome to Dream LMS Home. Browse and learn courses online with our full-stack Next.js course management app.",
+    type: "website",
+  },
+};
 const HomePage = () => {
     return (
         <div>
